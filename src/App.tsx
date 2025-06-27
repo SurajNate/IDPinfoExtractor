22cd0967177d177a305c6aafba6f9c52a41bbadf
Suraj Nate <surajnate29@gmail.com>
2025-06-28 01:23:10 +0530

    App.tsx commit

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
